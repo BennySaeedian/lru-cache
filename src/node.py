6aceb512eafbc5a4b8fbf65b08d0f12c697b4cc1
benyamin <benyaminsa@gmail.com>
2024-04-20 12:22:13 +0300

    fix node to supp dl-list

--- src/node.py
+++ src/node.py
@@ -7,9 +7,15 @@ T = TypeVar('T')
 
 class Node(Generic[T]):
 
-    def __init__(self, data: T, next_node: Node[T] | None = None) -> None:
+    def __init__(
+            self,
+            data: T,
+            next_node: Node[T] | None = None,
+            prev_node: Node[T] | None = None,
+    ) -> None:
         self.data = data
         self.next_node = next_node
+        self.prev_node = prev_node
 
     def __repr__(self) -> str:
         data = self.data
