854c9af79902e964cb2cbcef44dfcb6a4d3c97c0
benyamin <benyaminsa@gmail.com>
2024-04-20 12:18:08 +0300

    add pycharms idea to gitignore

---
+++ src/node.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import TypeVar, Generic
+
+T = TypeVar('T')
+
+
+class Node(Generic[T]):
+
+    def __init__(self, data: T, next_node: Node[T] | None = None) -> None:
+        self.data = data
+        self.next_node = next_node
+
+    def __repr__(self) -> str:
+        data = self.data
+        return f"Node({data=})"
